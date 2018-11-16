de75229d2befa2366fbf4e195afd160b40c06fc9
David MacLeod <macleod.david@live.co.uk>
2018-11-16 14:40:50 +0000

    Updated setup for Salamander.ai

--- ml_train/utils/tf_object_detection_setup.sh
+++ ml_train/utils/tf_object_detection_setup.sh
@@ -1,30 +1,47 @@
 pretrained_model=ssd_mobilenet_v1_coco_2018_01_28
 
+conda activate tensorflow
+
 # install dependencies
-apt-get install protobuf-compiler python-pil python-lxml python-tk
+sudo apt-get install protobuf-compiler python-pil python-lxml python-tk
 pip install Cython
 pip install jupyter
 pip install matplotlib
+pip install jpeg
+
+git clone https://github.com/david-macleod/mini-metro
 
 # clone object detection repo
 git clone https://github.com/tensorflow/models.git
 
 # clone cocoapi and run make
 git clone https://github.com/cocodataset/cocoapi.git
-cd cocoapi/PythonAPI; make; cp -r pycocotools /content/models/research/
+cd cocoapi/PythonAPI
+make
+cp -r pycocotools ~/models/research
 
-cd /content/models/research
+cd ~/models/research
 
 # add research/slim directions to PYTHONPATH
-%set_env PYTHONPATH=/content/models/research:/content/models/research/slim
+echo "export PYTHONPATH=$PYTHONPATH:~/models/research:~/models/research/slim" >> ~/.bashrc
 
 # compile protos
-protoc object_detection/protos/*.proto --python_out=.
+wget -O protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+unzip protobuf.zip
+./bin/protoc object_detection/protos/*.proto --python_out=.
 
 # run test
 python object_detection/builders/model_builder_test.py
 
 # get pre-trained model
+cd ~
 wget http://download.tensorflow.org/models/object_detection/${pretrained_model}.tar.gz
 tar -xvf ${pretrained_model}.tar.gz
-cp ${pretrained_model}/model.ckpt.* .
+mkdir -p model_zoo/${pretrained_model}
+cp ${pretrained_model}/model.ckpt.* model_zoo/${pretrained_model}/
+
+# clean up
+rm -rf ${pretrained_model}*
+
+# create train output directory
+mkdir ml_train
