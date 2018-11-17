d1d4ca9fc5e8483a465498b547d38326c8747e71
David MacLeod <macleod.david@live.co.uk>
2018-11-17 20:57:42 +0000

    Inital commit

---
+++ ml_train/station_detector/run.sh
@@ -0,0 +1,12 @@
+MODEL=$1
+CONFIG=$2
+PIPELINE_CONFIG_PATH="models/${MODEL}/${CONFIG}"
+
+
+print(PIPELINE_CONFIG_PATH)
+
+for i in {1...5}
+do
+  MODEL_DIR="/home/ubuntu/tf_data/station_detector/${MODEL}/train${i}"
+  python -m object_detection.model_main --model_dir=$MODEL_DIR --pipeline_config_path=$PIPELINE_CONFIG_PATH
+done
