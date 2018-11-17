28332eaa9e2d0273401d2fe87247a16bd77c3c8c
David MacLeod <macleod.david@live.co.uk>
2018-11-17 21:47:14 +0000

    Fixed syntax for range

--- ml_train/station_detector/run.sh
+++ ml_train/station_detector/run.sh
@@ -2,7 +2,7 @@ MODEL=$1
 CONFIG=$2
 PIPELINE_CONFIG_PATH="models/${MODEL}/${CONFIG}"
 
-for i in {1...5}
+for i in {1..5}
 do
   MODEL_DIR="/home/ubuntu/tf_data/station_detector/${MODEL}/train${i}"
   python -m object_detection.model_main --model_dir=$MODEL_DIR --pipeline_config_path=$PIPELINE_CONFIG_PATH
