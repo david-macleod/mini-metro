b6d1e443efe2b115021fb5421b46f83190e73830
David MacLeod <macleod.david@live.co.uk>
2018-11-17 22:29:45 +0000

    Adde multiple pretrained models

--- ml_train/utils/tf_object_detection_setup.sh
+++ ml_train/utils/tf_object_detection_setup.sh
@@ -1,7 +1,3 @@
-pretrained_model=ssd_mobilenet_v1_coco_2018_01_28
-
-source activate tensorflow
-
 # install dependencies
 sudo apt-get install protobuf-compiler python-pil python-lxml python-tk
 pip install Cython
@@ -20,28 +16,33 @@ cd cocoapi/PythonAPI
 make
 cp -r pycocotools ~/models/research
 
-cd ~/models/research
-
 # add research/slim directions to PYTHONPATH
 echo "export PYTHONPATH=$PYTHONPATH:~/models/research:~/models/research/slim" >> ~/.bashrc
+source ~/.bashrc
 
 # compile protos
+cd ~/models/research
 wget -O protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
 unzip protobuf.zip
 ./bin/protoc object_detection/protos/*.proto --python_out=.
 
 # run test
+source activate tensorflow
 python object_detection/builders/model_builder_test.py
 
-# get pre-trained model
+# get pre-trained models
 cd ~
-wget http://download.tensorflow.org/models/object_detection/${pretrained_model}.tar.gz
-tar -xvf ${pretrained_model}.tar.gz
-mkdir -p model_zoo/${pretrained_model}
-cp ${pretrained_model}/model.ckpt.* model_zoo/${pretrained_model}/
+declare -a PRETRAINED_MODELS=("ssd_mobilenet_v1_coco_2018_01_28" 
+"ssd_mobilenet_v1_fpn_shared_box_predictor_640x640_coco14_sync_2018_07_03" )
 
-# clean up
-rm -rf ${pretrained_model}*
+for pretrained_model in ${PRETRAINED_MODELS[*]}
+do
+  wget http://download.tensorflow.org/models/object_detection/${pretrained_model}.tar.gz
+  tar -xvf ${pretrained_model}.tar.gz
+  mkdir -p model_zoo/${pretrained_model}
+  cp ${pretrained_model}/model.ckpt.* model_zoo/${pretrained_model}/
+  rm -rf ${pretrained_model}*
+done
 
 # create train output directory
 mkdir tf_data
