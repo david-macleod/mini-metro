eca8535226260a7ebdc52db9e995f2c1df7e80f5
David MacLeod <macleod.david@live.co.uk>
2018-11-20 13:12:24 +0000

    Added latest model

--- plugins/SerpentMiniMetroGamePlugin/files/api/api.py
+++ plugins/SerpentMiniMetroGamePlugin/files/api/api.py
@@ -10,8 +10,8 @@ class MiniMetroAPI(GameAPI):
         super().__init__(game=game)
         self.ml_models_dir = offshoot.config["file_paths"]["game_ml_models"]
         self.ml_station_detector = ObjectDetector(
-            frozen_graph_path=f"{self.ml_models_dir}/station_detector_m0_frozen_graph.pb",
-            class_labels_path=f"{self.ml_models_dir}/station_detector_m0_label_map.json"
+            frozen_graph_path=f"{self.ml_models_dir}/station_detector_m6-1_frozen_graph.pb",
+            class_labels_path=f"{self.ml_models_dir}/station_detector_label_map.json"
         )
 
     def my_api_function(self):
