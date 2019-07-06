2b9268a9ef091cc8fe8fff6b0a5883fb7d9d69f8
David MacLeod <macleod.david@live.co.uk>
2019-07-06 07:33:54 +0100

    Added ptyolov3 home

--- ml_train/station_detector/pytorch/run.sh
+++ ml_train/station_detector/pytorch/run.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+PTYOLOV3_HOME=/home/ubuntu/PyTorch-YOLOv3
 MODEL_DIR=$1
+
 rm -rf "$MODEL_DIR"/logs
 rm -rf "$MODEL_DIR"/checkpoints
 mkdir "$MODEL_DIR"/logs
 mkdir "$MODEL_DIR"/checkpoints
-python -m ptyolov3.train --model_def "$MODEL_DIR"/yolov3-custom.cfg --data_config "$MODEL_DIR"/custom.data --pretrained_weights /home/ubuntu/PyTorch-YOLOv3/weights/darknet53.conv.74 --n_cpu 4 --epochs=21 --output_dir "$MODEL_DIR" --checkpoint_interval 5 --batch_size 10
+
+python -m ptyolov3.train --model_def "$MODEL_DIR"/yolov3-custom.cfg \
+                         --data_config "$MODEL_DIR"/custom.data \
+                         --pretrained_weights "$PTYOLOV3_HOME"/weights/darknet53.conv.74 \
+                         --n_cpu 4 \
+                         --epochs 21 \
+                         --output_dir "$MODEL_DIR" \
+                         --checkpoint_interval 5 \
+                         --batch_size 10
+                         /
